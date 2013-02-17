fc96fd53dee0943b46f3d0d997d0c1e7c621357d
Joe Borg <jdb@sitrep3.com>
2013-02-17 11:39:19 +0000

    Opening repo

---
+++ top_ten.py
@@ -0,0 +1,40 @@
+import urllib2, json
+from bs4 import BeautifulSoup
+
+class DistroWatchTopTen(object):
+    """
+    Get the top 10 Linux distros from DistroWatch
+    and make some exportable formats for it.
+    """
+    def __init__(self):
+        """
+        On init, create the soup.
+        """
+        url = "http://distrowatch.com/dwres.php?resource=major"
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page.read())
+        page.close()
+
+        self.top_ten = list()
+        dists = soup.find_all(attrs={'class':'News1'})
+        for dist in dists:
+            try:
+                obj = dict()
+                obj['name'] = str(dist.find(attrs={'class':'NewsHeadline'}).getText())
+                obj['icon'] = str("http://distrowatch.com/"+dist.find("img").attrs['src'])
+                obj['desc'] = str(dist.find(attrs={'class':'NewsText'}).getText())
+                self.top_ten.append(obj)
+            except:
+                continue
+
+    def toJson(self):
+        """
+        Convert the top 10 to a JSON object.
+        """
+        return json.dumps(self.top_ten)
+
+    def toPython(self):
+        """
+        Return the Python list of dicts.
+        """
+        return self.top_ten
